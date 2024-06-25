ce944ecb6beb3deb79418dd056da84b2d89a5956
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-06-25 16:23:18 +0900

    [Gold II] Title: 후위 표기식, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    static int priority(char op) {
+        if (op == '*' || op == '/') {
+            return 2;
+        } else if (op == '+' || op == '-') {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        char[] C = br.readLine().toCharArray();
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : C) {
+            if (Character.isLetter(c)) {
+                sb.append(c);
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    sb.append(stack.pop());
+                }
+
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                while (!stack.isEmpty() && priority(stack.peek()) >= priority(c)) {
+                    sb.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}

7155828aa4123907f63658637c4cb9033c1cc3ee
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-08-10 16:25:35 +0900

    탑.java

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -2,33 +2,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		var br = new BufferedReader(new InputStreamReader(System.in));
-		var sb = new StringBuilder();
-		
-		int N = Integer.parseInt(br.readLine());
-		var st = new StringTokenizer(br.readLine());
-		var stack = new ArrayDeque<int[]>();
-		
-		for (int i = 0; i < N; i++) {
-			int idx = 0;
-			int hei = Integer.parseInt(st.nextToken());
-			
-			while (stack.size() > 0) {
-				if (stack.peek()[1] > hei) {
-					idx = stack.peek()[0];
-					break;
-				}
-				stack.pop();
-			}
-			sb.append(idx).append(' ');
-			stack.push(new int[] {i + 1, hei});
-		}
-		System.out.println(sb);
-		br.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Deque<int[]> stack = new ArrayDeque<>();
+        
+        for (int i = 0; i < N; i++) {
+            int idx = 0;
+            int height = Integer.parseInt(st.nextToken());
+            
+            while (!stack.isEmpty()) {
+                if (stack.peek()[1] > height) {
+                    idx = stack.peek()[0];
+                    break;
+                }
+                stack.pop();
+            }
+            sb.append(idx).append(' ');
+            stack.push(new int[] {i + 1, height});
+        }
+        System.out.println(sb);
+        br.close();
+    }
 }

71e9c076a5ae82e37d981a829991d879a41cdca7
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-08-31 19:10:29 +0900

    Update 거북이.java

--- 백준/Silver/8911. 거북이/거북이.java
+++ 백준/Silver/8911. 거북이/거북이.java
@@ -8,61 +8,40 @@ public class Main {
 
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-        char[] head = {'N', 'E', 'S', 'W'};
 
         for (int i = 0; i < T; i++) {
             char[] command = br.readLine().toCharArray();
-            int len = command.length;
 
-            int x = 0, y = 0;
+            int x = 0, y = 0, head = 0;
             int minX = 0, maxX = 0;
             int minY = 0, maxY = 0;
-            int headIndex = 0;
 
-            for (int j = 0; j < len; j++) {
-                switch (command[j]) {
-                    case 'F':
-                        if (headIndex == 0) {
-                            y++;
-                            maxY = Math.max(maxY, y);
-                        } else if (headIndex == 1) {
-                            x++;
-                            maxX = Math.max(maxX, x);
-                        } else if (headIndex == 2) {
-                            y--;
-                            minY = Math.min(minY, y);
-                        } else {
-                            x--;
-                            minX = Math.min(minX, x);
-                        }
-                        break;
+            for (char c : command) {
+                if (c == 'L') {
+                    head = (head + 3) % 4;
+                } else if (c == 'R') {
+                    head = (head + 1) % 4;
+                } else {
+                    int move = (c == 'F') ? 1 : -1;
 
-                    case 'B':
-                        if (headIndex == 0) {
-                            y--;
-                            minY = Math.min(minY, y);
-                        } else if (headIndex == 1) {
-                            x--;
-                            minX = Math.min(minX, x);
-                        } else if (headIndex == 2) {
-                            y++;
-                            maxY = Math.max(maxY, y);
-                        } else {
-                            x++;
-                            maxX = Math.max(maxX, x);
-                        }
-                        break;
+                    if (head == 0) {
+                        y += move;
+                    } else if (head == 1) {
+                        x += move;
+                    } else if (head == 2) {
+                        y -= move;
+                    } else if (head == 3) {
+                        x -= move;
+                    }
 
-                    case 'L':
-                        headIndex = headIndex == 0 ? head.length - 1 : headIndex - 1;
-                        break;
-
-                    case 'R':
-                        headIndex = headIndex == head.length - 1 ? 0 : headIndex + 1;
-                        break;
+                    minX = Math.min(minX, x);
+                    maxX = Math.max(maxX, x);
+                    minY = Math.min(minY, y);
+                    maxY = Math.max(maxY, y);
                 }
             }
-            int result = (maxX + Math.abs(minX)) * (maxY + Math.abs(minY));
+
+            int result = (maxX - minX) * (maxY - minY);
             sb.append(result).append('\n');
         }
 

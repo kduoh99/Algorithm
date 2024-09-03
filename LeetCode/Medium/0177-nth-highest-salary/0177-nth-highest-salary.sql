5576f22b8615961b19186c8e10684a6269bf35d5
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-09-03 21:26:31 +0900

    Update 0177-nth-highest-salary.sql

--- LeetCode/Medium/0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ LeetCode/Medium/0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,5 +1,6 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
+    # RETURN 내에서 파라미터를 수정할 수 없다. SET이나 DECLARE를 사용하자.
     SET N = N - 1;
     RETURN (
         SELECT DISTINCT salary

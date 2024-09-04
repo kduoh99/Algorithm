d639eefee0d3f1dea3bd7f0996917ebccff0da4a
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-09-04 13:50:27 +0900

    Time: 6 ms (14.31%), Space: 46.1 MB (41.77%) - LeetHub

--- LeetCode/Hard/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ LeetCode/Hard/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,5 +1,7 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        final int SIZE = nums1.length + nums2.length;
+        
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         
         for (int num : nums1) {
@@ -9,30 +11,20 @@ class Solution {
         for (int num : nums2) {
             pq.offer(num);
         }
-        
-        final int SIZE = nums1.length + nums2.length;
-        int tmp = 0;
+
+        int midean = SIZE / 2;
         double result = 0;
         
         if (SIZE % 2 == 0) {
-            tmp = SIZE / 2 - 1;
-            
-            for (int i = 0; i < tmp; i++) {
-                pq.poll();
+            for (int i = 0; i < midean; i++) {
+                result = pq.poll();
             }
-            
-            double a = pq.poll();
-            double b = pq.poll();
-            
-            result = (a + b) / 2;
+            result += pq.peek();
+            result /= 2;
         } else {
-            tmp = SIZE / 2;
-            
-            for (int i = 0; i < tmp; i++) {
-                pq.poll();
+            for (int i = 0; i <= midean; i++) {
+                result = pq.poll();
             }
-            
-            result = pq.poll();
         }
         
         return result;

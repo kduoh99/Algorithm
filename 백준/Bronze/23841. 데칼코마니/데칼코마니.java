90bcc5d3c9eeb6a774b760ece91fe8538f532cd9
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-09-08 00:05:41 +0900

    Update 데칼코마니.java

--- 백준/Bronze/23841. 데칼코마니/데칼코마니.java
+++ 백준/Bronze/23841. 데칼코마니/데칼코마니.java
@@ -4,32 +4,44 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static final char DOT = '.';
+    private static int N, M;
+    private static char[][] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        char[][] arr = new char[N][M];
+        arr = new char[N][M];
         for (int i = 0; i < N; i++) {
             arr[i] = br.readLine().toCharArray();
         }
 
+        paint();
+        print();
+        br.close();
+    }
+
+    private static void paint() {
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (arr[i][j] != '.') {
+            for (int j = 0; j < M / 2; j++) {
+                if (arr[i][j] != DOT) {
                     arr[i][M - 1 - j] = arr[i][j];
+                } else if (arr[i][M - 1 - j] != DOT) {
+                    arr[i][j] = arr[i][M - 1 - j];
                 }
             }
         }
+    }
 
+    private static void print() {
         StringBuilder sb = new StringBuilder();
         for (char[] ch : arr) {
             sb.append(ch).append('\n');
         }
-
         System.out.println(sb);
-        br.close();
     }
 }

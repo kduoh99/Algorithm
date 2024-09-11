acc096e1307b8f6e8152b3c1c5aa4c0b761499ee
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-09-11 21:21:31 +0900

    [Silver IV] Title: 제로, Time: 192 ms, Memory: 23984 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -2,13 +2,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int K = Integer.parseInt(br.readLine());
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        Deque<Integer> stack = new ArrayDeque<>();
 
         for (int i = 0; i < K; i++) {
             int num = Integer.parseInt(br.readLine());

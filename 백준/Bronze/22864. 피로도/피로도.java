963d6cd4f84b2794b16aeea0c29c25d837873d41
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-09-22 22:03:15 +0900

    [Bronze II] Title: 피로도, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/22864. 피로도/피로도.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int hours = 0, works = 0, fatigue = 0;
+        while (hours < 24) {
+            if (A > M) {
+                System.out.println(0);
+                return;
+            }
+
+            if (fatigue + A <= M) {
+                fatigue += A;
+                works += B;
+            } else {
+                fatigue = Math.max((fatigue - C), 0);
+            }
+            hours++;
+        }
+
+
+        System.out.println(works);
+        br.close();
+    }
+}

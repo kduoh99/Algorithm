468b4e912f4d66d923e5792b0561332a1c264eaa
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-10-09 10:09:32 +0900

    [Silver IV] Title: 스택, Time: 156 ms, Memory: 18784 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> stack = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+
+            switch (cmd) {
+                case "push":
+                    int value = Integer.parseInt(st.nextToken());
+                    stack.push(value);
+                    break;
+                case "pop":
+                    sb.append(stack.isEmpty() ? -1 : stack.pop()).append('\n');
+                    break;
+                case "size":
+                    sb.append(stack.size()).append('\n');
+                    break;
+                case "empty":
+                    sb.append(stack.isEmpty() ? 1 : 0).append('\n');
+                    break;
+                case "top":
+                    sb.append(stack.isEmpty() ? -1 : stack.peek()).append('\n');
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}

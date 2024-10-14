b9a7b858bf322d4b7fcbf04f8241045828526d60
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-10-14 16:40:42 +0900

    [Silver I] Title: 음식물 피하기, Time: 136 ms, Memory: 15928 KB -BaekjoonHub

---
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final int[] dx = {-1, 0, 1, 0};
+    private static final int[] dy = {0, 1, 0, -1};
+    private static boolean[][] map;
+    private static boolean[][] visited;
+    private static int N, M, count, ans;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        map = new boolean[N][M];
+        visited = new boolean[N][M];
+
+        while (K-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            map[--r][--c] = true;
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (!visited[i][j] && map[i][j]) {
+                    count = 0;
+                    dfs(i, j);
+                    ans = Math.max(ans, count);
+                }
+            }
+        }
+
+        System.out.println(ans);
+        br.close();
+    }
+
+    private static void dfs(int x, int y) {
+        count++;
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int cx = x + dx[i];
+            int cy = y + dy[i];
+
+            if (cx >= 0 && cy >= 0 && cx < N && cy < M) {
+                if (!visited[cx][cy] && map[cx][cy]) {
+                    dfs(cx, cy);
+                }
+            }
+        }
+    }
+}

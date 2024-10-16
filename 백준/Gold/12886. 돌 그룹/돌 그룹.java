2170f12ba01bbe7af31ae9bc47def8b65c35de28
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-10-16 19:08:19 +0900

    [Gold IV] Title: 돌 그룹, Time: 204 ms, Memory: 35536 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.java
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.java
@@ -38,27 +38,29 @@ public class Main {
 
             if (a == b && b == c) return true;
 
+            // A와 B
             if (a < b && !visited[a + a][b - a]) {
                 q.offer(new int[]{a + a, b - a, c});
                 visited[a + a][b - a] = true;
-            }
-            if (a < c && !visited[a + a][c - a]) {
-                q.offer(new int[]{a + a, b, c - a});
-                visited[a + a][c - a] = true;
-            }
-            if (b < a && !visited[b + b][a - b]) {
+            } else if (a > b && !visited[b + b][a - b]) {
                 q.offer(new int[]{a - b, b + b, c});
                 visited[b + b][a - b] = true;
             }
-            if (b < c && !visited[b + b][c - b]) {
-                q.offer(new int[]{a, b + b, c - b});
-                visited[b + b][c - b] = true;
-            }
-            if (c < a && !visited[c + c][a - c]) {
+
+            // A와 C
+            if (a < c && !visited[a + a][c - a]) {
+                q.offer(new int[]{a + a, b, c - a});
+                visited[a + a][c - a] = true;
+            } else if (a > c && !visited[c + c][a - c]) {
                 q.offer(new int[]{a - c, b, c + c});
                 visited[c + c][a - c] = true;
             }
-            if (c < b && !visited[c + c][b - c]) {
+
+            // B와 C
+            if (b < c && !visited[b + b][c - b]) {
+                q.offer(new int[]{a, b + b, c - b});
+                visited[b + b][c - b] = true;
+            } else if (b > c && !visited[c + c][b - c]) {
                 q.offer(new int[]{a, b - c, c + c});
                 visited[c + c][b - c] = true;
             }

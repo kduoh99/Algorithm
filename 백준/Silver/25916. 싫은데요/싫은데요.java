20cfbc8985bae5098e86d989e4238226028a6f55
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-11-04 15:42:00 +0900

    [Silver I] Title: 싫은데요, Time: 392 ms, Memory: 66676 KB -BaekjoonHub

---
+++ 백준/Silver/25916. 싫은데요/싫은데요.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0, end = 0, sum = 0, max = 0;
+        while (end < N) {
+            sum += arr[end++];
+
+            while (sum > M) {
+                sum -= arr[start++];
+            }
+            max = Math.max(max, sum);
+        }
+
+        System.out.println(max);
+        br.close();
+    }
+}

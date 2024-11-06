38f8881638f79b3db5de6d5ab284075b283b36f3
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-11-06 13:54:14 +0900

    Update 돌 굴러가유.java

--- 백준/Gold/23889. 돌 굴러가유/돌 굴러가유.java
+++ 백준/Gold/23889. 돌 굴러가유/돌 굴러가유.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,44 +13,40 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
+        // 누적합 배열
         st = new StringTokenizer(br.readLine());
-        long[] X = new long[N + 1];
+        int[] X = new int[N + 1];
         for (int i = 1; i <= N; i++) {
             X[i] = X[i - 1] + Integer.parseInt(st.nextToken());
         }
 
+        // 돌 위치
         st = new StringTokenizer(br.readLine());
         int[] S = new int[K];
         for (int i = 0; i < K; i++) {
             S[i] = Integer.parseInt(st.nextToken());
         }
 
-        List<long[]> V = new ArrayList<>();
+        // 파괴된 모래성
+        int[][] arr = new int[K][2];
         for (int i = 0; i < K - 1; i++) {
-            long destroy = X[S[i + 1] - 1] - X[S[i] - 1];
-            V.add(new long[]{destroy, S[i]});
+            arr[i] = new int[]{X[S[i + 1] - 1] - X[S[i] - 1], S[i]};
         }
+        arr[K - 1] = new int[] {X[N] - X[S[K - 1] - 1], S[K - 1]};
 
-        int lastS = S[K - 1];
-        long lastD = X[N] - X[lastS - 1];
-        V.add(new long[]{lastD, lastS});
+        // 파괴량 내림차순 정렬
+        Arrays.sort(arr, (o1, o2) -> o2[0] - o1[0]);
 
-        V.sort((o1, o2) -> {
-            if (o1[0] == o2[0]) {
-                return Long.compare(o1[1], o2[1]);
-            }
-            return Long.compare(o2[0], o1[0]);
-        });
-
-        List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < Math.min(M, V.size()); i++) {
-            answer.add((int) V.get(i)[1]);
+        // 상위 M개의 위치, 오름차순 정렬
+        int[] W = new int[M];
+        for (int i = 0; i < M; i++) {
+            W[i] = arr[i][1];
         }
+        Arrays.sort(W);
 
-        Collections.sort(answer);
         StringBuilder sb = new StringBuilder();
-        for (int x : answer) {
-            sb.append(x).append('\n');
+        for (int w : W) {
+            sb.append(w).append('\n');
         }
 
         System.out.println(sb);

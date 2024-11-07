e665b36ad34b3aefdd4c1155ee3fd83c9c54dcd9
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-11-07 14:14:05 +0900

    [Gold V] Title: 다각형의 면적, Time: 176 ms, Memory: 19116 KB -BaekjoonHub

--- 백준/Gold/2166. 다각형의 면적/다각형의 면적.java
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.java
@@ -8,7 +8,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[][] xy = new int[N + 1][2];
+        int[][] xy = new int[N][2];
 
         StringTokenizer st;
         for (int i = 0; i < N; i++) {
@@ -16,14 +16,12 @@ public class Main {
             xy[i][0] = Integer.parseInt(st.nextToken());
             xy[i][1] = Integer.parseInt(st.nextToken());
         }
-
-        xy[N][0] = xy[0][0];
-        xy[N][1] = xy[0][1];
+        // 신발끈 공식
         long sumA = 0L, sumB = 0L;
-
         for (int i = 0; i < N; i++) {
-            sumA += (long) xy[i][0] * xy[i + 1][1];
-            sumB += (long) xy[i + 1][0] * xy[i][1];
+            int next = (i + 1) % N;
+            sumA += (long) xy[i][0] * xy[next][1];
+            sumB += (long) xy[next][0] * xy[i][1];
         }
 
         System.out.printf("%.1f%n", Math.abs(sumA - sumB) / 2.0);

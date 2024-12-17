2eaee565f0a4fcdd612119f912e81677fb001198
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-17 13:41:08 +0900

    [Gold IV] Title: 두 배, Time: 376 ms, Memory: 39364 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.java
+++ 백준/Gold/31963. 두 배/두 배.java
@@ -8,29 +8,47 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] A = new int[N + 1];
-
-		for (int i = 1; i <= N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		int count = 0;
+		int[] next = new int[N];
+		for (int i = 1; i < N - 1; i++) {
+			int tmp = arr[i];
 
-		while (true) {
-			boolean flag = false;
-			for (int i = 1; i <= N; i++) {
-				while (A[i] < A[i - 1]) {
-					A[i] *= 2;
-					count++;
-					flag = true;
-				}
+			while (tmp <= arr[i + 1]) {
+				tmp <<= 1;
+				next[i]++;
 			}
-			if (!flag)
-				break;
+
+			if (next[i] > 0) {
+				next[i]--;
+			}
 		}
 
-		System.out.println(count);
+		int[] prev = new int[N + 1];
+		for (int i = 1; i < N; i++) {
+			int tmp = arr[i];
+
+			while (tmp < arr[i - 1]) {
+				tmp <<= 1;
+				prev[i]++;
+			}
+
+			if (next[i] < prev[i]) {
+				prev[i + 1] += prev[i] - next[i];
+			}
+		}
+
+		long count = 0;
+		for (int r : prev) {
+			count += r;
+		}
+
+		System.out.println(count - prev[N]);
 		br.close();
 	}
 }

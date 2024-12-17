b226e613da01c8f4c5cc5356a351d645590ad986
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-17 14:11:50 +0900

    Update 두 배.java

--- 백준/Gold/31963. 두 배/두 배.java
+++ 백준/Gold/31963. 두 배/두 배.java
@@ -8,45 +8,41 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
+		int[] A = new int[N];
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+		for (int i = 0; i < N; i++)
+			A[i] = Integer.parseInt(st.nextToken());
 
 		int[] next = new int[N];
 		for (int i = 1; i < N - 1; i++) {
-			int tmp = arr[i];
+			int tmp = A[i];
 
-			while (tmp <= arr[i + 1]) {
+			while (tmp <= A[i + 1]) {
 				tmp <<= 1;
 				next[i]++;
 			}
 
-			if (next[i] > 0) {
+			if (next[i] > 0)
 				next[i]--;
-			}
 		}
 
 		int[] prev = new int[N + 1];
 		for (int i = 1; i < N; i++) {
-			int tmp = arr[i];
+			int tmp = A[i];
 
-			while (tmp < arr[i - 1]) {
+			while (tmp < A[i - 1]) {
 				tmp <<= 1;
 				prev[i]++;
 			}
 
-			if (next[i] < prev[i]) {
+			if (next[i] < prev[i])
 				prev[i + 1] += prev[i] - next[i];
-			}
 		}
 
-		long count = 0;
-		for (int r : prev) {
-			count += r;
-		}
+		long count = 0L;
+		for (int p : prev)
+			count += p;
 
 		System.out.println(count - prev[N]);
 		br.close();

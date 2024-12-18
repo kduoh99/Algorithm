db664a8ffb122fe7168715f62dba49f16c2fe149
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-18 22:17:38 +0900

    [Silver II] Title: 시계, Time: 124 ms, Memory: 15448 KB -BaekjoonHub

--- 백준/Silver/8989. 시계/시계.java
+++ 백준/Silver/8989. 시계/시계.java
@@ -1,48 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static final int SIZE = 5;
+	private static final int SIZE = 5;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < T; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String[] times = new String[SIZE];
 
-            PriorityQueue<String> pq = new PriorityQueue<>(
-                    Comparator.comparingDouble((String time) -> {
-                        String[] hhmm = time.split(":");
-                        int hh = Integer.parseInt(hhmm[0]);
-                        int mm = Integer.parseInt(hhmm[1]);
-                        return calculateAngle(hh, mm);
-                    }).thenComparing(time -> time)
-            );
+			for (int j = 0; j < SIZE; j++) {
+				times[j] = st.nextToken();
+			}
 
-            for (int j = 0; j < SIZE; j++) {
-                pq.offer(st.nextToken());
-            }
+			Arrays.sort(times, (a, b) -> {
+				double angleA = calAngle(a);
+				double angleB = calAngle(b);
 
-            pq.poll();
-            pq.poll();
-            sb.append(pq.poll()).append('\n');
-        }
+				if (angleA != angleB) {
+					return Double.compare(angleA, angleB);
+				}
+				return a.compareTo(b);
+			});
+			sb.append(times[2]).append('\n');
+		}
 
-        System.out.println(sb);
-        br.close();
-    }
+		System.out.println(sb);
+		br.close();
+	}
 
-    private static double calculateAngle(int hh, int mm) {
-        double hhAngle = (hh % 12) * 30 + (mm * 0.5);
-        double mmAngle = mm * 6;
-        double angle = Math.abs(hhAngle - mmAngle);
-        return Math.min(angle, 360 - angle);
-    }
+	private static double calAngle(String time) {
+		String[] hm = time.split(":");
+		int h = Integer.parseInt(hm[0]);
+		int m = Integer.parseInt(hm[1]);
+		double hAngle = (h % 12) * 30 + (m * 0.5);
+		double mAngle = m * 6;
+		double angle = Math.abs(hAngle - mAngle);
+		return Math.min(angle, 360 - angle);
+	}
 }

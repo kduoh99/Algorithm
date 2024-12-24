da613bf9efc2fcddf28b940a17632b45f00cb20d
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-24 09:51:14 +0900

    [Silver II] Title: 이상한 하노이 탑, Time: 124 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Silver/15500. 이상한 하노이 탑/이상한 하노이 탑.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static final StringBuilder sb = new StringBuilder();
+	private static int K = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Deque<Integer> stack1 = new ArrayDeque<>();
+		Deque<Integer> stack2 = new ArrayDeque<>();
+
+		for (int i = 0; i < N; i++) {
+			stack1.push(Integer.parseInt(st.nextToken()));
+		}
+
+		int[] disk = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			disk[i] = 1;
+		}
+
+		for (int i = N; i >= 1; i--) {
+			if (disk[i] == 1) {
+				while (stack1.peek() != i) {
+					int v = stack1.pop();
+					stack2.push(v);
+					disk[v] = 2;
+					move(1, 2);
+				}
+				stack1.pop();
+				move(1, 3);
+			} else if (disk[i] == 2) {
+				while (stack2.peek() != i) {
+					int v = stack2.pop();
+					stack1.push(v);
+					disk[v] = 1;
+					move(2, 1);
+				}
+				stack2.pop();
+				move(2, 3);
+			}
+		}
+
+		System.out.println(K);
+		System.out.println(sb);
+		br.close();
+	}
+
+	private static void move(int A, int B) {
+		sb.append(A).append(' ').append(B).append('\n');
+		K++;
+	}
+}

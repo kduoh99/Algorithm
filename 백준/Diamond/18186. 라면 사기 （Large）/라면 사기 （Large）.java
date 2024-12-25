81b91c5246375987982071b277144cd35414d5c9
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-25 22:56:18 +0900

    Update 라면 사기 （Large）.java

--- 백준/Diamond/18186. 라면 사기 （Large）/라면 사기 （Large）.java
+++ 백준/Diamond/18186. 라면 사기 （Large）/라면 사기 （Large）.java
@@ -21,28 +21,29 @@ public class Main {
 
 		long cost = 0L;
 		for (int i = 0; i < N; i++) {
-			if (C <= B && i < N - 2 && A[i + 1] > A[i + 2]) {
-				int pack = Math.min(A[i], A[i + 1] - A[i + 2]);
-				cost += pack * (B + C);
-				A[i] -= pack;
-				A[i + 1] -= pack;
-			}
+			if (C <= B) {
+				if (i < N - 2 && A[i + 1] > A[i + 2]) {
+					int pack = Math.min(A[i], A[i + 1] - A[i + 2]);
+					cost += pack * (B + C);
+					A[i] -= pack;
+					A[i + 1] -= pack;
+				}
 
-			if (B + 2 * C < 3 * B && i < N - 2) {
-				int pack = Math.min(A[i], Math.min(A[i + 1], A[i + 2]));
-				cost += pack * (B + 2L * C);
-				A[i] -= pack;
-				A[i + 1] -= pack;
-				A[i + 2] -= pack;
-			}
+				if (i < N - 2) {
+					int pack = Math.min(A[i], Math.min(A[i + 1], A[i + 2]));
+					cost += pack * (B + 2L * C);
+					A[i] -= pack;
+					A[i + 1] -= pack;
+					A[i + 2] -= pack;
+				}
 
-			if (B + C < 2 * B && i < N - 1) {
-				int pack = Math.min(A[i], A[i + 1]);
-				cost += pack * (B + C);
-				A[i] -= pack;
-				A[i + 1] -= pack;
+				if (i < N - 1) {
+					int pack = Math.min(A[i], A[i + 1]);
+					cost += pack * (B + C);
+					A[i] -= pack;
+					A[i + 1] -= pack;
+				}
 			}
-
 			cost += A[i] * B;
 		}
 

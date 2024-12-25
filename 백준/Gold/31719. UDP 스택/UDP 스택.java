934915c581cb881b74bc653b66fa8162eed58d89
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-25 09:48:07 +0900

    [Gold IV] Title: UDP 스택, Time: 364 ms, Memory: 46192 KB -BaekjoonHub

---
+++ 백준/Gold/31719. UDP 스택/UDP 스택.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+
+		while (T-- > 0) {
+			int N = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int last = 0, dTop = 0, dBottom = 0, pTop = 0, pBottom = 0;
+			boolean flag = false;
+
+			for (int i = 0; i < N; i++) {
+				int v = Integer.parseInt(st.nextToken());
+
+				if (dBottom == last + 1) {
+					last = dTop;
+					dBottom = dTop = 0;
+				}
+
+				if (pBottom == last + 1) {
+					last = pTop;
+					pBottom = pTop = 0;
+				}
+
+				if (v == last + 1) {
+					last++;
+				} else if (dTop == v - 1) {
+					dTop++;
+				} else if (pTop == v - 1) {
+					pTop++;
+				} else if (dBottom == 0) {
+					dBottom = dTop = v;
+				} else if (pBottom == 0) {
+					pBottom = pTop = v;
+				} else {
+					flag = true;
+					break;
+				}
+			}
+
+			sb.append(flag ? "NO" : "YES").append('\n');
+		}
+
+		System.out.println(sb);
+		br.close();
+	}
+}

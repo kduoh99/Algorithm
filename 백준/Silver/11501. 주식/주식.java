95051bbb815ad77c64216cdd1c8a233fe872dbcd
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 20:20:07 +0900

    Update 주식.java

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -5,36 +5,35 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine());
-            int[] A = new int[N];
-            st = new StringTokenizer(br.readLine());
+		while (T-- > 0) {
+			int N = Integer.parseInt(br.readLine());
+			int[] A = new int[N];
+			StringTokenizer st = new StringTokenizer(br.readLine());
 
-            for (int j = 0; j < N; j++) {
-                A[j] = Integer.parseInt(st.nextToken());
-            }
+			for (int j = 0; j < N; j++) {
+				A[j] = Integer.parseInt(st.nextToken());
+			}
 
-            long maxProfit = 0;
-            int maxPrice = 0;
+			long maxProfit = 0;
+			int maxPrice = 0;
 
-            for (int k = N - 1; k >= 0; k--) {
-                if (A[k] > maxPrice) {
-                    maxPrice = A[k];
-                } else {
-                    maxProfit += maxPrice - A[k];
-                }
-            }
-            sb.append(maxProfit).append('\n');
-        }
+			for (int k = N - 1; k >= 0; k--) {
+				if (A[k] > maxPrice) {
+					maxPrice = A[k];
+				} else {
+					maxProfit += maxPrice - A[k];
+				}
+			}
+			sb.append(maxProfit).append('\n');
+		}
 
-        System.out.println(sb);
-        br.close();
-    }
+		System.out.println(sb);
+		br.close();
+	}
 }

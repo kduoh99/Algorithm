8e988528ee185b214da82456ff43882dc840ad2e
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 21:08:16 +0900

    [Gold V] Title: 용액 합성하기, Time: 304 ms, Memory: 25472 KB -BaekjoonHub

--- 백준/Gold/14921. 용액 합성하기/용액 합성하기.java
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.java
@@ -4,35 +4,35 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
+		int N = Integer.parseInt(br.readLine());
+		int[] A = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-        }
+		int left = 0, right = N - 1;
+		int ans = Integer.MAX_VALUE;
 
-        int left = 0, right = N - 1;
-        int ans = Integer.MAX_VALUE;
+		while (left < right) {
+			int sum = A[left] + A[right];
 
-        while (left < right) {
-            int sum = A[left] + A[right];
+			if (Math.abs(ans) > Math.abs(sum)) {
+				ans = sum;
+			}
 
-            if (Math.abs(ans) > Math.abs(sum)) {
-                ans = sum;
-            }
+			if (sum < 0) {
+				left++;
+			} else {
+				right--;
+			}
+		}
 
-            if (sum < 0) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-
-        System.out.println(ans);
-        br.close();
-    }
+		System.out.println(ans);
+		br.close();
+	}
 }

11f4a473e52ae0e6d087b11d80c67bf8c5cf129a
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 20:26:00 +0900

    [Bronze V] Title: 아스키 코드, Time: 112 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String input = br.readLine();
+		bw.write(Integer.toString(input.charAt(0)) + "\n");
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}

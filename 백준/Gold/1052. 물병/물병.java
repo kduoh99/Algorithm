a6d41f6b5a41bc741c25aa328bb7e59a5024b287
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 09:17:52 +0900

    [Gold V] Title: 물병, Time: 116 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Gold/1052. 물병/물병.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		int ans = 0;
+		while (Integer.bitCount(N) > K) {
+			ans++;
+			N++;
+		}
+
+		System.out.println(ans);
+		br.close();
+	}
+}

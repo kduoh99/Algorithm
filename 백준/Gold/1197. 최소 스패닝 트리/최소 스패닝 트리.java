b071bc001a121f5bdbf0ad1a685752c2458cc833
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 10:00:45 +0900

    Update 최소 스패닝 트리.java

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,60 +2,61 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int ans = 0, V, E;
-    static List<int[]>[] graph;
-    static boolean[] visited;
+	private static int V;
+	private static List<int[]>[] graph;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        graph = new ArrayList[V + 1];
-        visited = new boolean[V + 1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		graph = new ArrayList[V + 1];
 
-        for (int i = 1; i <= V; i++)
-            graph[i] = new ArrayList<>();
+		for (int i = 1; i <= V; i++)
+			graph[i] = new ArrayList<>();
 
-        for (int i = 1; i <= E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            int C = Integer.parseInt(st.nextToken());
+		for (int i = 1; i <= E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int C = Integer.parseInt(st.nextToken());
 
-            graph[A].add(new int[] {B, C});
-            graph[B].add(new int[] {A, C});
-        }
+			graph[A].add(new int[] {B, C});
+			graph[B].add(new int[] {A, C});
+		}
 
-        prim(1);
-        System.out.println(ans);
-    }
+		System.out.println(prim());
+		br.close();
+	}
 
-    static void prim(int start) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        pq.add(new int[] {start, 0});
+	private static int prim() {
+		int ans = 0;
+		PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
+		boolean[] visited = new boolean[V + 1];
+		pq.offer(new int[] {1, 0});
 
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int node = cur[0];
-            int weight = cur[1];
+		while (!pq.isEmpty()) {
+			int[] cur = pq.poll();
+			int node = cur[0];
+			int weight = cur[1];
 
-            if (visited[node])
-                continue;
+			if (visited[node]) continue;
+			visited[node] = true;
+			ans += weight;
 
-            visited[node] = true;
-            ans += weight;
+			for (int[] next : graph[node]) {
+				if (!visited[next[0]])
+					pq.offer(next);
+			}
+		}
 
-            for (int[] next : graph[node]) {
-                if (!visited[next[0]])
-                    pq.add(next);
-            }
-        }
-    }
+		return ans;
+	}
 }

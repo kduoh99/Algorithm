eb4388c5e31bd80b899991e1f713b3c5e9cba0ea
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 11:46:58 +0900

    [Gold IV] Title: 타일 채우기, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        dp[0] = 1;
+        dp[1] = 0;
+
+        for (int i = 2; i <= N; i += 2) {
+            dp[i] = dp[i - 2] * 3;
+
+            for (int j = i - 4; j >= 0; j -= 2) {
+                dp[i] += dp[j] * 2;
+            }
+        }
+
+        System.out.println(dp[N]);
+        br.close();
+    }
+}

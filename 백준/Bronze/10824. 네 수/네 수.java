78390f0f4b1cffd9112911889275451d6036cd6f
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 19:38:24 +0900

    [Bronze III] Title: 네 수, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		String AB = st.nextToken() + st.nextToken();
+		String CD = st.nextToken() + st.nextToken();
+
+		long ans = Long.parseLong(AB) + Long.parseLong(CD);
+		System.out.println(ans);
+		br.close();
+	}
+}

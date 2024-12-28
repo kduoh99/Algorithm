27a7051ff787713fc731c650b5cc7aab45253554
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 20:30:05 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 548 ms, Memory: 54088 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		int[] sum = new int[N + 1];
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 1; i <= N; i++)
+			sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			sb.append(sum[end] - sum[start - 1]).append('\n');
+		}
+		
+		System.out.println(sb);
+		br.close();
+	}
+}

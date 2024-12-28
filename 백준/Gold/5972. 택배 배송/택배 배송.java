59ba61e9168df40ee110848f3eea5765076165f0
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 18:43:46 +0900

    Update 택배 배송.java

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -4,53 +4,53 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static ArrayList<int[]>[] graph;
+	private static int N;
+	private static ArrayList<int[]>[] graph;
 
-    static int Dijkstra(int start) {
-        int[] dist = new int[N + 1];
-        int INF = Integer.MAX_VALUE;
-        Arrays.fill(dist, INF);
-        dist[start] = 0;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
-        pq.offer(new int[] {start, 0});
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
-        while (!pq.isEmpty()) {
-            int curIdx = pq.poll()[0];
+		graph = new ArrayList[N + 1];
+		for (int i = 0; i <= N; i++)
+			graph[i] = new ArrayList<>();
 
-            for (int[] next : graph[curIdx]) {
-                if (dist[next[0]] > dist[curIdx] + next[1]) {
-                    dist[next[0]] = dist[curIdx] + next[1];
-                    pq.offer(new int[] {next[0], dist[next[0]]});
-                }
-            }
-        }
-        return dist[N];
-    }
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			graph[a].add(new int[] {b, c});
+			graph[b].add(new int[] {a, c});
+		}
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+		System.out.println(dijkstra(1));
+		br.close();
+	}
 
-        graph = new ArrayList[N + 1];
-        for (int i = 0; i <= N; i++)
-            graph[i] = new ArrayList<>();
+	private static int dijkstra(int start) {
+		int[] dist = new int[N + 1];
+		int INF = Integer.MAX_VALUE;
+		Arrays.fill(dist, INF);
+		dist[start] = 0;
 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
+		PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
+		pq.offer(new int[] {start, 0});
 
-            graph[a].add(new int[] {b, c});
-            graph[b].add(new int[] {a, c});
-        }
+		while (!pq.isEmpty()) {
+			int curIdx = pq.poll()[0];
 
-        System.out.println(Dijkstra(1));
-        br.close();
-    }
+			for (int[] next : graph[curIdx]) {
+				if (dist[next[0]] > dist[curIdx] + next[1]) {
+					dist[next[0]] = dist[curIdx] + next[1];
+					pq.offer(new int[] {next[0], dist[next[0]]});
+				}
+			}
+		}
+		return dist[N];
+	}
 }

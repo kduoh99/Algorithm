0aeb4c3655085238151abf909718a3b088a1bc46
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 19:49:59 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 120 ms, Memory: 15876 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String str = br.readLine();
+		int d = 1;
+
+		for (int i = 0; i < str.length() / 2; ++i) {
+			if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+				d = 0;
+				break;
+			}
+		}
+
+		bw.write(d + "\n");
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}

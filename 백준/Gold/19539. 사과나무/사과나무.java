1d03674bda02894dc7417d599bad4f4b130af99c
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 21:56:18 +0900

    [Gold V] Title: 사과나무, Time: 244 ms, Memory: 23912 KB -BaekjoonHub

--- 백준/Gold/19539. 사과나무/사과나무.java
+++ 백준/Gold/19539. 사과나무/사과나무.java
@@ -4,27 +4,26 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int one = 0, two = 0, sum = 0;
-        String ans = "YES";
+		int one = 0, two = 0, sum = 0;
+		String ans = "YES";
 
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			sum += num;
+			one += num % 2;
+			two += num / 2;
+		}
 
-            sum += num;
-            one += num % 2;
-            two += num / 2;
-        }
+		if (sum % 3 > 0 || one > two)
+			ans = "NO";
 
-        if (sum % 3 > 0 || one > two)
-            ans = "NO";
-
-        System.out.println(ans);
-        br.close();
-    }
+		System.out.println(ans);
+		br.close();
+	}
 }

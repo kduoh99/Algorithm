501ce694714954c7378a258d766d66662d8eb71e
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 21:31:52 +0900

    Update Hashing.java

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -3,24 +3,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		var br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int L = Integer.parseInt(br.readLine());
 		int r = 31;
 		int M = 1234567891;
-		
+
 		String s = br.readLine();
 		char[] C = s.toCharArray();
-		
+
 		long ans = 0;
 		long pow = 1;
+		
 		for (int i = 0; i < L; i++) {
-			byte A = (byte) C[i]; 
+			byte A = (byte) C[i];
 			ans += (A - 96) * pow;
 			pow = (pow * r) % M;
 		}
+		
 		System.out.print(ans % M);
 		br.close();
 	}

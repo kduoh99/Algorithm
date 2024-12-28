176c1fa0c04ae4235adc53839d04cfbc59b7c4c8
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 22:18:36 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 944 ms, Memory: 102636 KB -BaekjoonHub

---
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static ArrayList<Integer>[] graph;
+	private static boolean[] visited;
+	private static int[] depth;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+
+		graph = new ArrayList[N + 1];
+		visited = new boolean[N + 1];
+		depth = new int[N + 1];
+
+		for (int i = 1; i <= N; i++)
+			graph[i] = new ArrayList<>();
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+
+			graph[u].add(v);
+			graph[v].add(u);
+		}
+
+		for (int i = 1; i <= N; i++)
+			graph[i].sort(Comparator.reverseOrder());
+
+		dfs(R);
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 1; i <= N; i++) {
+			if (!visited[i])
+				sb.append(-1).append('\n');
+			else
+				sb.append(depth[i]).append('\n');
+		}
+
+		System.out.println(sb);
+		br.close();
+	}
+
+	private static void dfs(int R) {
+		Deque<int[]> stack = new ArrayDeque<>();
+		stack.push(new int[] { R, 0 });
+
+		while (!stack.isEmpty()) {
+			int[] cur = stack.pop();
+			int node = cur[0];
+			int nDepth = cur[1];
+
+			if (!visited[node]) {
+				visited[node] = true;
+				depth[node] = nDepth;
+
+				for (int next : graph[node])
+					stack.push(new int[] { next, nDepth + 1 });
+			}
+		}
+	}
+}

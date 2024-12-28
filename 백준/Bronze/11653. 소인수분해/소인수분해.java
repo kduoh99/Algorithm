43da74b6a8f5d2cdd8c5f060657296bd579dfce0
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 20:27:12 +0900

    Update 소인수분해.java

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -3,26 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		var br = new BufferedReader(new InputStreamReader(System.in));
-		var sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		StringBuilder sb = new StringBuilder();
 		long N = Integer.parseInt(br.readLine());
 
 		int num = 2;
 		while (true) {
-			if (N == 1)
-				break;
-			
+			if (N == 1) break;
 			if (N % num == 0) {
 				N /= num;
 				sb.append(num).append("\n");
-			}
-			else
+			} else {
 				num++;
+			}
 		}
-		System.out.println(sb.toString());
+		
+		System.out.println(sb);
 		br.close();
 	}
 }

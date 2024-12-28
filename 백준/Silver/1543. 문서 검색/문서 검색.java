610bee4573f5db72014e05efa0a8c9d8ee88f3bb
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 10:41:51 +0900

    [Silver V] Title: 문서 검색, Time: 108 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+		String search = br.readLine();
+		int cnt = 0, idx = 0;
+
+		while ((idx = str.indexOf(search, idx)) != -1) {
+			cnt++;
+			idx += search.length();
+		}
+
+		System.out.println(cnt);
+		br.close();
+	}
+}

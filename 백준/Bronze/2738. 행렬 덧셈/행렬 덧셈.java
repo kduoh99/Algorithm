17713300402c6489456fb39c4cc5ddd57eb968a3
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 17:51:08 +0900

    Update 행렬 덧셈.java

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -13,27 +13,24 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		int [][] A = new int[N][M];
-		int [][] B = new int[N][M];
 		int [][] result = new int[N][M];
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
-				A[i][j] = Integer.parseInt(st.nextToken());
+				result[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
-				B[i][j] = Integer.parseInt(st.nextToken());
+				result[i][j] += Integer.parseInt(st.nextToken());
 			}
 		}
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				result[i][j] = A[i][j] + B[i][j];
 				bw.write(result[i][j] + " ");
 			}
 			bw.write("\n");

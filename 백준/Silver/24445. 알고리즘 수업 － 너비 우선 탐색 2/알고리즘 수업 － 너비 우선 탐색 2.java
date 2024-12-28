7a19145bf45eea1ac84916c4258c7998a8185910
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 22:13:17 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 912 ms, Memory: 90928 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static ArrayList<Integer>[] graph;
+	private static boolean[] visited;
+	private static int[] order;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+
+		graph = new ArrayList[N + 1];
+		visited = new boolean[N + 1];
+		order = new int[N + 1];
+
+		for (int i = 1; i <= N; i++)
+			graph[i] = new ArrayList<>();
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+
+			graph[u].add(v);
+			graph[v].add(u);
+		}
+
+		bfs(N, R);
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 1; i <= N; i++)
+			sb.append(order[i]).append('\n');
+
+		System.out.println(sb);
+		br.close();
+	}
+
+	private static void bfs(int N, int R) {
+		int val = 1;
+		Deque<Integer> q = new ArrayDeque<>();
+		q.offer(R);
+		visited[R] = true;
+		order[R] = val++;
+
+		while (!q.isEmpty()) {
+			int cur = q.poll();
+			Collections.sort(graph[cur], Collections.reverseOrder());
+
+			for (int next : graph[cur]) {
+				if (!visited[next]) {
+					q.offer(next);
+					visited[next] = true;
+					order[next] = val++;
+				}
+			}
+		}
+	}
+}

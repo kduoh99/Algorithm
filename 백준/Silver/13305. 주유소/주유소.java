c7b8ba8a3dcea478f9c06b4cb02d6bab5939a508
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 20:50:49 +0900

    Update 주유소.java

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -4,31 +4,30 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		var br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int N = Integer.parseInt(br.readLine());
-		var D = new long[N - 1];
-		var P = new long[N];
-		
-		var st = new StringTokenizer(br.readLine());
+		long[] D = new long[N - 1];
+		long[] P = new long[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N - 1; i++)
 			D[i] = Integer.parseInt(st.nextToken());
-		
+
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++)
 			P[i] = Integer.parseInt(st.nextToken());
-		
+
 		long ans = 0;
 		long minP = P[0];
 
 		for (int i = 0; i < N - 1; i++) {
 			if (minP > P[i])
 				minP = P[i];
-			
 			ans += minP * D[i];
-		}	
+		}
+		
 		System.out.println(ans);
 		br.close();
 	}

7ebb6984cbcc854c0cfd62dae9f7ff4e7a19b8e3
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 19:52:48 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 108 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= N; i++) {
+			sb.append(" ".repeat(Math.max(0, N - i)));
+
+			for (int j = 1; j <= 2 * i - 1; j++)
+				sb.append(j % 2 == 0 ? ' ' : '*');
+			sb.append('\n');
+		}
+
+		System.out.println(sb);
+		br.close();
+	}
+}

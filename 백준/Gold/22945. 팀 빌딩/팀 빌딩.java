1902f747e4e83d65c70845c2ed394af56238a230
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 22:02:33 +0900

    [Gold IV] Title: 팀 빌딩, Time: 256 ms, Memory: 24228 KB -BaekjoonHub

--- 백준/Gold/22945. 팀 빌딩/팀 빌딩.java
+++ 백준/Gold/22945. 팀 빌딩/팀 빌딩.java
@@ -4,32 +4,32 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int[] X = new int[N];
+		int N = Integer.parseInt(br.readLine());
+		int[] X = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			X[i] = Integer.parseInt(st.nextToken());
+		}
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            X[i] = Integer.parseInt(st.nextToken());
-        }
+		int left = 0, right = N - 1;
+		int ans = 0;
 
-        int left = 0, right = N - 1;
-        int ans = 0;
+		while (left < right) {
+			int status = (right - left - 1) * Math.min(X[left], X[right]);
+			ans = Math.max(ans, status);
 
-        while (left < right) {
-            int status = (right - left - 1) * Math.min(X[left], X[right]);
-            ans = Math.max(ans, status);
+			if (X[left] < X[right]) {
+				left++;
+			} else {
+				right--;
+			}
+		}
 
-            if (X[left] < X[right]) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-
-        System.out.println(ans);
-        br.close();
-    }
+		System.out.println(ans);
+		br.close();
+	}
 }

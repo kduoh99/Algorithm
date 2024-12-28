de2fa9766af733ef9b4f1278bf968dbb71f6887f
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 19:18:01 +0900

    [Bronze IV] Title: 평균 점수, Time: 96 ms, Memory: 14016 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int sum = 0;
+		for (int i = 0; i < 5; i++) {
+			int num = Integer.parseInt(br.readLine());
+
+			if (num < 40)
+				num = 40;
+			sum += num;
+		}
+		
+		System.out.println(sum / 5);
+		br.close();
+	}
+}

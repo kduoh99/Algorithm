1846bcf0d9cae455590f02840048de4b868ef0fe
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2024-12-28 21:34:23 +0900

    Update 이상한 기호.java

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -3,14 +3,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
-		var br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		String[] input = br.readLine().split(" ");
 		long A = Integer.parseInt(input[0]);
 		long B = Integer.parseInt(input[1]);
-		
+
 		System.out.println((A + B) * (A - B));
 		br.close();
 	}

a1cb8024ac729b14aec8099db8c8914fb502fb3e
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2025-01-03 00:25:34 +0900

    [Silver II] Title: 서버실, Time: 544 ms, Memory: 95576 KB -BaekjoonHub

--- 백준/Silver/17245. 서버실/서버실.java
+++ 백준/Silver/17245. 서버실/서버실.java
@@ -4,45 +4,51 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static int N;
+	private static int[][] map;
+	private static long total;
 
-        int N = Integer.parseInt(br.readLine());
-        int[][] map = new int[N][N];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st;
-        long total = 0;
-        int max = 0;
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+		total = 0L;
+		int max = 0;
 
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                total += map[i][j];
-                max = Math.max(max, map[i][j]);
-            }
-        }
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int left = 0, right = max;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            long count = 0;
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				total += map[i][j];
+				max = Math.max(max, map[i][j]);
+			}
+		}
 
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    count += Math.min(mid, map[i][j]);
-                }
-            }
+		System.out.println(binarySearch(0, max));
+		br.close();
+	}
 
-            if (count * 2 >= total) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
+	private static int binarySearch(int left, int right) {
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			long count = 0;
 
-        System.out.println(left);
-        br.close();
-    }
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					count += Math.min(mid, map[i][j]);
+				}
+			}
+
+			if (count * 2 >= total) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+
+		return left;
+	}
 }

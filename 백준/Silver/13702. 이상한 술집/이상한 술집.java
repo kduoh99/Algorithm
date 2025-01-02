f0ae28973aa1292fd192eb4cc2d1770ad62df7db
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2025-01-03 00:16:29 +0900

    [Silver II] Title: 이상한 술집, Time: 148 ms, Memory: 17204 KB -BaekjoonHub

--- 백준/Silver/13702. 이상한 술집/이상한 술집.java
+++ 백준/Silver/13702. 이상한 술집/이상한 술집.java
@@ -4,38 +4,44 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static int K;
+	private static int[] arr;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] arr = new int[N];
-        int max = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            max = Math.max(max, arr[i]);
-        }
+		arr = new int[N];
+		int max = 0;
 
-        long left = 1, right = max;
-        while (left <= right) {
-            long mid = (left + right) / 2;
-            int count = 0;
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			max = Math.max(max, arr[i]);
+		}
 
-            for (int a : arr) {
-                count += a / mid;
-            }
+		System.out.println(binarySearch(1L, max));
+		br.close();
+	}
 
-            if (count >= K) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
+	private static long binarySearch(long left, long right) {
+		while (left <= right) {
+			long mid = (left + right) / 2;
+			int count = 0;
 
-        System.out.println(right);
-        br.close();
-    }
+			for (int a : arr) {
+				count += a / mid;
+			}
+
+			if (count >= K) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+
+		return right;
+	}
 }

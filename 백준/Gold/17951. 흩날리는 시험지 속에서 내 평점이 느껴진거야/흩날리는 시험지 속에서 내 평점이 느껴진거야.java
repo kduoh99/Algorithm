1beeca6f83f123b855665416852e14ccdd0822bd
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2025-01-03 00:22:36 +0900

    Update 흩날리는 시험지 속에서 내 평점이 느껴진거야.java

--- 백준/Gold/17951. 흩날리는 시험지 속에서 내 평점이 느껴진거야/흩날리는 시험지 속에서 내 평점이 느껴진거야.java
+++ 백준/Gold/17951. 흩날리는 시험지 속에서 내 평점이 느껴진거야/흩날리는 시험지 속에서 내 평점이 느껴진거야.java
@@ -4,43 +4,50 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static int K;
+	private static int[] X;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        st = new StringTokenizer(br.readLine());
-        int[] X = new int[N];
-        int left = Integer.MAX_VALUE, right = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++) {
-            X[i] = Integer.parseInt(st.nextToken());
-            left = Math.min(left, X[i]);
-            right += X[i];
-        }
+		st = new StringTokenizer(br.readLine());
+		X = new int[N];
+		int left = Integer.MAX_VALUE, right = 0;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int sum = 0, count = 0;
+		for (int i = 0; i < N; i++) {
+			X[i] = Integer.parseInt(st.nextToken());
+			left = Math.min(left, X[i]);
+			right += X[i];
+		}
 
-            for (int x : X) {
-                sum += x;
-                if (sum >= mid) {
-                    sum = 0;
-                    count++;
-                }
-            }
+		System.out.println(binarySearch(left, right));
+		br.close();
+	}
 
-            if (count >= K) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
+	private static int binarySearch(int left, int right) {
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			int sum = 0, count = 0;
 
-        System.out.println(right);
-        br.close();
-    }
+			for (int x : X) {
+				sum += x;
+				if (sum >= mid) {
+					sum = 0;
+					count++;
+				}
+			}
+
+			if (count >= K) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+
+		return right;
+	}
 }

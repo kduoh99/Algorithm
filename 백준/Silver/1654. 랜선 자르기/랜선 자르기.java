284a13f4a0cba2688b0bc6b054f66dfbbb89ff15
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2025-01-03 00:01:43 +0900

    Update 랜선 자르기.java

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -5,36 +5,40 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static int K, N;
+	private static int[] arr;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int K = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
-		int[] arr = new int[K];
+		K = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		arr = new int[K];
 
 		for (int i = 0; i < K; i++)
 			arr[i] = Integer.parseInt(br.readLine());
 
 		Arrays.sort(arr);
-		long max = arr[K - 1];
-		long min = 1, mid;
+		System.out.println(binarySearch(1, arr[K - 1]));
+		br.close();
+	}
 
-		while (min <= max) {
-			mid = (max + min) / 2;
+	private static long binarySearch(long left, long right) {
+		while (left <= right) {
+			long mid = (left + right) / 2;
 			long cnt = 0;
 
 			for (int i = 0; i < K; i++)
 				cnt += arr[i] / mid;
 
-			if (cnt < N) {
-				max = mid - 1;
+			if (cnt >= N) {
+				left = mid + 1;
 			} else {
-				min = mid + 1;
+				right = mid - 1;
 			}
 		}
 
-		System.out.println(max);
-		br.close();
+		return right;
 	}
 }

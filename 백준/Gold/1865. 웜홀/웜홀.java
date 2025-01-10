cd6fd38a36af957392c81eb0aadf3071b2b17674
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2025-01-10 19:31:17 +0900

    [Gold III] Title: 웜홀, Time: 304 ms, Memory: 22088 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -24,15 +24,13 @@ public class Main {
 			N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
 			int W = Integer.parseInt(st.nextToken());
-
-			dist = new int[N + 1];
 			edges = new ArrayList<>();
 
-			for (int j = 1; j <= N; j++) {
-				edges.add(new Edge(0, j, 0));
+			for (int i = 1; i <= N; i++) {
+				edges.add(new Edge(0, i, 0));
 			}
 
-			for (int j = 0; j < M; j++) {
+			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
 				int S = Integer.parseInt(st.nextToken());
 				int E = Integer.parseInt(st.nextToken());
@@ -41,7 +39,7 @@ public class Main {
 				edges.add(new Edge(E, S, T));
 			}
 
-			for (int j = 0; j < W; j++) {
+			for (int i = 0; i < W; i++) {
 				st = new StringTokenizer(br.readLine());
 				int S = Integer.parseInt(st.nextToken());
 				int E = Integer.parseInt(st.nextToken());
@@ -57,15 +55,21 @@ public class Main {
 	}
 
 	private static boolean bellmanFord() {
+		dist = new int[N + 1];
 		Arrays.fill(dist, INF);
 		dist[0] = 0;
 
 		for (int i = 1; i < N; i++) {
+			boolean flag = false;
+
 			for (Edge edge : edges) {
 				if (dist[edge.from] != INF && dist[edge.to] > dist[edge.from] + edge.weight) {
 					dist[edge.to] = dist[edge.from] + edge.weight;
+					flag = true;
 				}
 			}
+
+			if (!flag) break;
 		}
 
 		for (Edge edge : edges) {

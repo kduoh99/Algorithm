7f771beaed3c175f1dc100a867932c765dca6545
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2025-01-17 11:42:46 +0900

    [Gold II] Title: 컬러볼, Time: 1740 ms, Memory: 92012 KB -BaekjoonHub

---
+++ 백준/Gold/10800. 컬러볼/컬러볼.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		Ball[] balls = new Ball[N];
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int c = Integer.parseInt(st.nextToken());
+			int s = Integer.parseInt(st.nextToken());
+			balls[i] = new Ball(i, c, s);
+		}
+
+		Arrays.sort(balls, Comparator.comparingInt(o -> o.size));
+		long[] colorSum = new long[N + 1];
+		long totalSum = 0L;
+		int idx = 0;
+
+		for (int i = 0; i < N; i++) {
+			while (idx < N && balls[idx].size < balls[i].size) {
+				totalSum += balls[idx].size;
+				colorSum[balls[idx].color] += balls[idx].size;
+				idx++;
+			}
+			balls[i].sum = totalSum - colorSum[balls[i].color];
+		}
+
+		Arrays.sort(balls, Comparator.comparingInt(o -> o.idx));
+		StringBuilder sb = new StringBuilder();
+
+		for (Ball ball : balls) {
+			sb.append(ball.sum).append('\n');
+		}
+
+		System.out.println(sb);
+		br.close();
+	}
+}
+
+class Ball {
+	int idx, color, size;
+	long sum;
+
+	public Ball(int idx, int color, int size) {
+		this.idx = idx;
+		this.color = color;
+		this.size = size;
+	}
+}

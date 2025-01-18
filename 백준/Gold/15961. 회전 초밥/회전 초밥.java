5917ba4845b3772ad6972bd580da92a0dc70fcac
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2025-01-18 13:05:16 +0900

    [Gold IV] Title: 회전 초밥, Time: 488 ms, Memory: 170864 KB -BaekjoonHub

---
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int[] belt = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			belt[i] = Integer.parseInt(br.readLine());
+		}
+
+		int[] check = new int[d + 1];
+		int answer, count = 0;
+
+		for (int i = 0; i < k; i++) {
+			if (check[belt[i]]++ == 0)
+				count++;
+		}
+		answer = check[c] == 0 ? count + 1 : count;
+
+		for (int i = 0; i < N; i++) {
+			if (--check[belt[i]] == 0)
+				count--;
+			
+			if (check[belt[(i + k) % N]]++ == 0)
+				count++;
+
+			answer = Math.max(answer, check[c] == 0 ? count + 1 : count);
+		}
+
+		System.out.println(answer);
+		br.close();
+	}
+}

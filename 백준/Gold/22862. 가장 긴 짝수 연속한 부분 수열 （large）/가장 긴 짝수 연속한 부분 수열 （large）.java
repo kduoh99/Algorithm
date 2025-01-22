cf4f9f4cc20594e4709e4745cab16498504497f2
Kang Duoh <143769463+kduoh99@users.noreply.github.com>
2025-01-22 09:24:15 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 440 ms, Memory: 91280 KB -BaekjoonHub

---
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		int[] S = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			S[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int left = 0, right = 0, count = 0, answer = 0;
+
+		while (right < N) {
+			if (S[right] % 2 == 0) {
+				right++;
+			} else if (count < K) {
+				count++;
+				right++;
+			} else {
+				if (S[left] % 2 != 0) {
+					count--;
+				}
+				left++;
+			}
+			answer = Math.max(answer, right - left - count);
+		}
+
+		System.out.println(answer);
+		br.close();
+	}
+}

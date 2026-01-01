6abb98cbc10213a353c980767c3f0bfe314d127b
Kang Duoh <kduoh99@gmail.com>
2026-01-02 00:48:01 +0900

    Time: 0 ms (100.00%), Space: 47.64 MB (98.62%) - LeetHub

--- LeetCode/Easy/0344-reverse-string/0344-reverse-string.java
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.java
@@ -1,12 +1,14 @@
 class Solution {
     public void reverseString(char[] s) {
         int left = 0, right = s.length - 1;
-        
+
         while (left < right) {
-            char tmp = s[left];
-            s[left] = s[right];
-            s[right] = tmp;
-            
+            if (s[left] != s[right]) {
+                char tmp = s[left];
+                s[left] = s[right];
+                s[right] = tmp;
+            }
+
             left++;
             right--;
         }

985b49e7f53d47e9b1fd89da2f429d1aca1ee661
Kang Duoh <kduoh99@gmail.com>
2026-01-04 13:51:59 +0900

    Time: 9 ms (94.20%), Space: 43.73 MB (97.62%) - LeetHub

--- LeetCode/Easy/0819-most-common-word/0819-most-common-word.java
+++ LeetCode/Easy/0819-most-common-word/0819-most-common-word.java
@@ -1,28 +1,32 @@
+import java.util.*;
+
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
-        // 특수문자 공백으로 대체, 소문자로 변환, 단어로 분리
-        String[] words = paragraph.replaceAll("\\W+", " ").toLowerCase().split("\\s+");
+        // 공백과 명시된 기호를 구분자로 지정
+        String delimiters = " !?',;.";
+        StringTokenizer st = new StringTokenizer(paragraph.toLowerCase(), delimiters);
         
-        // 금지 단어들을 저장할 Set
-        Set<String> ban = new HashSet<>();
-        for (String word : banned) {
-            ban.add(word);
-        }
-        
-        // 단어 빈도를 저장할 Map
-        Map<String, Integer> wordCount = new HashMap<>();
-        for (String word : words) {
-            if (!ban.contains(word)) {
-                // 해당 단어가 Map에 이미 존재하면 기존 값에 1을 더하고, 그렇지 않으면 1을 저장
-                wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        // 단어 빈도 저장할 Map
+        Map<String, Integer> map = new HashMap<>();
+
+        while (st.hasMoreTokens()) {
+            boolean flag = false;
+            String word = st.nextToken();
+
+            for (String ban : banned) {
+                if (word.equals(ban)) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            // 해당 단어가 Map에 존재하면 기존 값에 1을 더하고, 그렇지 않으면 1을 저장
+            if (!flag) {
+                map.put(word, map.getOrDefault(word, 0) + 1);
             }
         }
-        
+
         // 가장 빈도 높은 단어 찾기
-        return wordCount.entrySet()
-            .stream()
-            .max(Map.Entry.comparingByValue())
-            .get()
-            .getKey();
+        return Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 }

37ac8042273ba01851ba6066581ee8ed79792c4c
Kang Duoh <kduoh99@gmail.com>
2026-01-05 17:31:36 +0900

    Time: 8 ms (42.90%), Space: 49.79 MB (50.29%) - LeetHub

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.java
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.java
@@ -1,19 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
         Map<String, List<String>> map = new HashMap<>();
-        
+
         for (String str : strs) {
-            char[] ch = str.toCharArray();
-            Arrays.sort(ch);
-            
-            String key = new String(ch);
-            
+            char[] key = str.toCharArray();
+            Arrays.sort(key);
+
             // 현재 키에 해당하는 리스트가 없다면 새로 생성, 있다면 기존 리스트에 추가
-            map.computeIfAbsent(key, k -> new ArrayList<>())
-                .add(str);
+            map.computeIfAbsent(String.valueOf(key), k -> new ArrayList<>()).add(str);
         }
-        
+
         // HashMap의 값들을 리스트로 반환
         return new ArrayList<>(map.values());
     }

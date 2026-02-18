08f0d46bc0996fe95a7b5c317878a96912828a6a
Kang Duoh <kduoh99@gmail.com>
2026-02-18 14:17:10 +0900

    Time: 0 ms (100%), Space: 42.5 MB (92.41%) - LeetHub

---
+++ LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.kt
@@ -0,0 +1,14 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun maxDepth(root: TreeNode?): Int =
+        if (root == null) 0 else maxOf(maxDepth(root.left), maxDepth(root.right)) + 1
+}
